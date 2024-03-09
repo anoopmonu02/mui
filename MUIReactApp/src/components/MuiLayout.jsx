e6d99f68ec6ca103fcf0849c6cac269243704549
Anoop <anoopmonu02@gmail.com>
2024-03-09 11:58:42 +0530

    MUI Grid and Paper

--- MUIReactApp/src/components/MuiLayout.jsx
+++ MUIReactApp/src/components/MuiLayout.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import { Box, Stack, Divider } from '@mui/material'
+import { Box, Stack, Divider, Grid, Paper } from '@mui/material'
 
 function MuiLayout() {
   return (
     /* direction='column-reverse' */
-    <Stack 
+    //Stack used for 1 dimensional layout
+    //Grid used for 2 dimensional layout
+    <Paper sx={{padding: '20px', margin: '20px'}} elevation={4}>
+        <Stack 
         spacing={2} 
         sx={{border: '1px solid'}} 
         direction='row'
         divider={<Divider orientation='vertical' flexItem />}
-    >
+        >
         <Box sx={{
             backgroundColor: 'primary.main',    
             color: 'white',
@@ -26,9 +29,19 @@ function MuiLayout() {
         <Box display='flex' height='100px' width='100px' bgcolor='success.light' p={2}>
             BOX-P stands for p
         </Box>
+        </Stack>
+        {/* //For equally distribution of items no interger value assign to xs, sm, md, lg, xl */}
+        <Grid container my={4}>
+            <Grid item xs={6}><Box bgcolor='primary.light' p={2}>Item 1</Box></Grid>
+            <Grid item xs={6}><Box bgcolor='primary.light' p={2}>Item 2</Box></Grid>
+            <Grid item xs={6}><Box bgcolor='primary.light' p={2}>Item 3</Box></Grid>
+            <Grid item xs={6}><Box bgcolor='primary.light' p={2}>Item 4</Box></Grid>
+            <Grid item xs={3}><Box bgcolor='primary.light' p={2}>Item 5</Box></Grid>
+            <Grid item xs={9}><Box bgcolor='primary.light' p={2}>Item 6</Box></Grid>
+        </Grid>
 
-
-    </Stack>
+    </Paper>
+    
     
   )
 }
